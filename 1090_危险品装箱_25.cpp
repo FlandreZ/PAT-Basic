83af7f3a53695404a615d7f5334bf0642506321d
yyz <2522797673@qq.com>
2019-11-28 17:17:53 +0800

    搜索时有一个小优化

---
+++ 1090_危险品装箱_25.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+int main(){
+    int m, n;
+    cin >> n >> m;
+    map<int, vector<int>> mp;
+    for (int i = 0; i < n;i++){
+        int a, b;
+        scanf("%d%d", &a, &b);
+        mp[a].push_back(b);
+        mp[b].push_back(a);
+    }
+    for (int i = 0; i < m;i++){
+        int k;
+        bool flag = true;
+        scanf("%d", &k);
+        int a[k],b[100000] = {0};
+        for (int j = 0; j < k;j++){
+            scanf("%d", a + j);
+            b[a[j]] = 1;
+        }
+        for (int j = 0; j < k;j++&&flag)
+            for (int l = 0; l < mp[a[j]].size();l++&&flag)
+                if(b[mp[a[j]][l]] == 1)
+                    flag = false;
+        if(flag)
+            printf("Yes\n");
+        else
+            printf("No\n");
+    }
+    return 0;
+}
